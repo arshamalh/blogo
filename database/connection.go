381e75f3a72fa5fea4d4c3dbbc0053c411f2c959
arshamalh <arshamalh@gmail.com>
2022-06-07 01:13:34 +0430

    [added]: Post model.

--- database/connection.go
+++ database/connection.go
@@ -21,7 +21,7 @@ func Connect(dsn string) {
 	fmt.Println("Database connection successfully opened")
 
 	// Auto migration
-	err = DB.AutoMigrate(models.User{})
+	err = DB.AutoMigrate(models.User{}, models.Post{})
 	if err != nil {
 		panic("Failed to migrate the database")
 	}
