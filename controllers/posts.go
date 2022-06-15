52ddef154f2e98d727f0b9c8fc571c5590dcccf9
arshamalh <arshamalh@gmail.com>
2022-06-15 19:15:31 +0430

    CreatePost error handlings improved.

--- controllers/posts.go
+++ controllers/posts.go
@@ -15,31 +15,38 @@ type PostRequest struct {
 	Categories []string `form:"categories" json:"categories"`
 }
 
-func CreatePost(c *gin.Context) {
+func CreatePost(ctx *gin.Context) {
 	var post PostRequest
-	user_id, _ := tools.ExtractUserID(c)
-	if c.BindJSON(&post) == nil {
-		catgs := []models.Category{}
-		for _, catg := range post.Categories {
-			catgs = append(catgs, models.Category{Name: catg})
-		}
-
-		new_post := models.Post{
-			Title:      post.Title,
-			Content:    post.Content,
-			AuthorID:   user_id,
-			Categories: catgs,
-		}
-		post_id, _ := database.CreatePost(&new_post)
-		c.JSON(200, gin.H{
-			"message": "Post created successfully",
-			"post_id": post_id,
-		})
+	user_id, err := tools.ExtractUserID(ctx)
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
 	}
+	if err := ctx.BindJSON(&post); err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	catgs := []models.Category{}
+	for _, catg := range post.Categories {
+		catgs = append(catgs, models.Category{Name: catg})
+	}
+
+	new_post := models.Post{
+		Title:      post.Title,
+		Content:    post.Content,
+		AuthorID:   user_id,
+		Categories: catgs,
+	}
+	post_id, _ := database.CreatePost(&new_post)
+	ctx.JSON(200, gin.H{
+		"message": "Post created successfully",
+		"post_id": post_id,
+	})
 }
 
 func GetPost(c *gin.Context) {
-	post_id, _ := strconv.Atoi(c.Param("id"))
+	post_id, _ := strconv.ParseUint(c.Param("id"), 10, 64)
 	post, _ := database.GetPost(uint(post_id))
 	c.JSON(200, gin.H{
 		"post": post,
