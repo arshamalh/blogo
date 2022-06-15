ad141b3d4ed513cdf12e02e8a0b8c0287d68f5ef
arshamalh <arshamalh@gmail.com>
2022-06-15 18:50:04 +0430

    permission numbers changed for good.

--- models/permissions/permissions.go
+++ models/permissions/permissions.go
@@ -11,31 +11,29 @@ const (
 	// Full access means this user can do anything with other users, their posts, and categories.
 	FullAccess Permission = 0
 
-	// Anyone who has this permission has the ability to invoke others permissions or give them higher permissions.
-	// There are just a few limitations about deleting users and like that.
-	AddAndChangeRoles Permission = 1
+	// Permission 1 to 20 are reserved for future use.
 
 	// Full content access means this user can make or delete any post or category or comment, but can't do anything with other users.
-	FullContents Permission = 2
+	FullContents Permission = 20
 
 	// Permission to make new posts and be an author.
-	CreatePost Permission = 3
+	CreatePost Permission = 21
 
 	// Users with this permission can edit others posts.
-	EditPost Permission = 4
+	EditPost Permission = 22
 
 	// Users with this permission can delete others posts.
-	DeletePost Permission = 5
+	DeletePost Permission = 23
 
 	// Permission to create new categories.
-	CreateCategory Permission = 6
+	CreateCategory Permission = 24
 
 	// Users with this permission can edit others categories.
-	EditCategory Permission = 7
+	EditCategory Permission = 25
 
 	// Users with this permission can delete others categories.
-	DeleteCategory Permission = 8
+	DeleteCategory Permission = 26
 
 	// Permission to approve or reject comments.
-	ApproveComment Permission = 9
+	ApproveComment Permission = 27
 )
