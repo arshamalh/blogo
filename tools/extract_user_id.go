6875fd4f3a30acb3c613b96e98a9aec4c1d562ec
arshamalh <arshamalh@gmail.com>
2022-06-15 19:17:37 +0430

    function doc added.

--- tools/extract_user_id.go
+++ tools/extract_user_id.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ExtractUserID extracts user ID from the request.
+// returns error if there is no user ID or if the user ID is not a number.
 func ExtractUserID(ctx *gin.Context) (uint, error) {
 	user_id_str, exists := ctx.Get("user_id")
 	if !exists {
